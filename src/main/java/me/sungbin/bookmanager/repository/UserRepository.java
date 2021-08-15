3a39ec7387fef4872974a0dd3d9e05863bc3a0c9
SungbinYang <ysb05222@naver.com>
2021-08-15 19:14:28 +0900

    쿼리메서드의 정의

--- src/main/java/me/sungbin/bookmanager/repository/UserRepository.java
+++ src/main/java/me/sungbin/bookmanager/repository/UserRepository.java
@@ -2,8 +2,33 @@ package me.sungbin.bookmanager.repository;
 
 import me.sungbin.bookmanager.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+import java.util.Set;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Set<User> findByName(String name);
+
+    User findByEmail(String email);
+
+    User getByEmail(String email);
+
+    User readByEmail(String email);
+
+    User queryByEmail(String email);
+
+    User searchByEmail(String email);
+
+    User streamByEmail(String email);
+
+    User findUserByEmail(String email);
+
+    User findSomethingByEmail(String email);
+
+    List<User> findFirst2ByName(String name);
+
+    List<User> findTop2ByName(String name);
+
+    List<User> findLast1ByName(String name);
 }
--- src/test/java/me/sungbin/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/me/sungbin/bookmanager/repository/UserRepositoryTest.java
@@ -20,4 +20,23 @@ class UserRepositoryTest {
 
         userRepository.save(user);
     }
+
+    @Test
+    void select() {
+        System.out.println(userRepository.findByName("dennis"));
+
+        System.out.println("findByEmail: " + userRepository.findByEmail("robert@google.com"));
+        System.out.println("getByEmail: " + userRepository.getByEmail("robert@google.com"));
+        System.out.println("readByEmail: " + userRepository.readByEmail("robert@google.com"));
+        System.out.println("queryByEmail: " + userRepository.queryByEmail("robert@google.com"));
+        System.out.println("searchByEmail: " + userRepository.searchByEmail("robert@google.com"));
+        System.out.println("streamByEmail: " + userRepository.streamByEmail("robert@google.com"));
+        System.out.println("findUserByEmail: " + userRepository.findUserByEmail("robert@google.com"));
+        System.out.println("findSomethingByEmail: " + userRepository.findSomethingByEmail("robert@google.com"));
+
+        System.out.println("findTop2ByName: " + userRepository.findTop2ByName("robert"));
+        System.out.println("findFirst2ByName: " + userRepository.findFirst2ByName("robert"));
+        System.out.println("findLast1ByName: " + userRepository.findLast1ByName("robert"));
+
+    }
 }
