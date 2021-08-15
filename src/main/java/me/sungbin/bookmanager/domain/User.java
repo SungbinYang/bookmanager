a8e62138ff6e9109326639ecb3c029730982d352
SungbinYang <ysb05222@naver.com>
2021-08-15 11:46:30 +0900

    Lombok 알아보기

---
+++ src/main/java/me/sungbin/bookmanager/domain/User.java
@@ -0,0 +1,23 @@
+package me.sungbin.bookmanager.domain;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
+public class User {
+
+    @NonNull
+    private String name;
+
+    @NonNull
+    private String email;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
---
+++ src/test/java/me/sungbin/bookmanager/domain/UserTest.java
@@ -0,0 +1,29 @@
+package me.sungbin.bookmanager.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void test() {
+        User user = new User();
+        user.setEmail("robert@gmail.com");
+        user.setName("robert");
+        user.setCreatedAt(LocalDateTime.now());
+        user.setUpdatedAt(LocalDateTime.now());
+
+        User user1 = new User("sungbin", "sungbin@gmail.com", LocalDateTime.now(), LocalDateTime.now());
+        User user2 = new User("robert", "robert@aaa.com");
+
+        User user3 = User.builder().name("robert").email("robert@gmail.com").build();
+
+        System.out.println(">>> " + user);
+        System.out.println(">>> " + user1);
+        System.out.println(">>> " + user2);
+        System.out.println(">>> " + user3);
+    }
+}
