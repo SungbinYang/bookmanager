5b8cb4dcb8ff69734e3292a2dac020c0826c703e
SungbinYang <ysb05222@naver.com>
2021-08-17 22:19:14 +0900

    영속성 컨텍스트 및 테스트 수정 및 DB mysql로 이관

--- src/test/java/me/sungbin/bookmanager/controller/HelloWorldControllerTest.java
+++ src/test/java/me/sungbin/bookmanager/controller/HelloWorldControllerTest.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
--- src/test/java/me/sungbin/bookmanager/repository/BookReviewInfoRepositoryTest.java
+++ src/test/java/me/sungbin/bookmanager/repository/BookReviewInfoRepositoryTest.java
@@ -21,8 +21,8 @@ class BookReviewInfoRepositoryTest {
     @Test
     void crudTest() {
         BookReviewInfo bookReviewInfo = new BookReviewInfo();
-
 //        bookReviewInfo.setBookId(1L);
+        bookReviewInfo.setBook(givenBook());
         bookReviewInfo.setAverageReviewScore(4.5f);
         bookReviewInfo.setReviewCount(2);
 
